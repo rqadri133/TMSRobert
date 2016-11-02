76f1c6451808b94d33be3f705a8a1efa1afd35ca
Rafi Qadri <rqadri133@gmail.com>
2016-11-02 12:02:45 -0500

    Add project files.

---
+++ TestFrameworkPortal/app/app.js
@@ -0,0 +1,58 @@
+﻿
+'use strict';
+
+
+var app;
+(function () {
+
+    app = angular.module("TFApp", ['ng', 'ngMaterial', 'ngMessages' ]).run(function ($rootScope) {
+        $rootScope.IsLogin = false;
+        $rootScope.parentObj = {};
+        $rootScope.parentObj.afterLogin = false;
+        $rootScope.parentObj.beforeLogin = true;
+
+        $rootScope.users = [];
+      
+
+        $rootScope.token = null;
+        $rootScope.ValidationCommonThread = [];
+        $rootScope.expressionAuthCode = ""  ;
+        $rootScope.UserID = "";
+
+        $rootScope.ValidationExpressions = {
+            expressionbuilderObj: $rootScope.ValidationCommonThread,
+            expressionAuthentication: $rootScope.expressionAuthCode 
+            
+        };
+
+        $rootScope.testConnectionTypes = [];
+
+    
+        $rootScope.testConnectionsAll = [];
+
+        $rootScope.testTableSources = [];
+
+        $rootScope.testColumnsSources = [];
+        $rootScope.testOperands = [];
+        
+
+        $rootScope.testConnectionObj = {
+            TestConnectionTypeID: "",
+            TestConnectionName: "",
+            TestConnectionString: "",
+            CreatedBy: ""
+        };
+
+        
+
+    });
+
+
+
+
+
+}
+
+        )();
+
+
